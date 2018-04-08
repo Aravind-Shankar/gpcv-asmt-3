596fba201128f76e61ef81334c019fa4356680d6
Aravind-Shankar <shankaravind167@gmail.com>
2018-04-08 23:17:28 +0530

    fix: use abs value in get_inliers, now new bugs arise.

--- src/get_inliers.m
+++ src/get_inliers.m
@@ -2,7 +2,7 @@ function [ ind, cnt ] = get_inliers( F, x, xp, tol )
 %COUNT_INLIERS Summary of this function goes here
 %   Detailed explanation goes here
 
-[ind,vals] = find(diag(xp * F * x') <= tol);
+[ind,vals] = find(abs(diag(xp * F * x')) <= tol);
 cnt = sum(vals);
 
 end
